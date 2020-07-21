ef75cf8732df7c240e813226900ce2ddc6ec93d9
lastblackstone <f.amriddinzoda@gmail.com>
2020-07-21 14:29:59 -0500

    added selenium grid server

--- src/test/java/com/VyTrack/utilities/Driver.java
+++ src/test/java/com/VyTrack/utilities/Driver.java
@@ -3,9 +3,18 @@ package com.VyTrack.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Driver {
     private static WebDriver driver;
+    private static ChromeOptions chromeOptions;
+    private static URL url;
+    private static ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
 
     private Driver() {
 
@@ -17,6 +26,15 @@ public class Driver {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
+                    break;
+                case "remote-chrome":
+                    chromeOptions = new ChromeOptions();
+                    try {
+                        URL url = new URL ("http://100.24.15.65:4444/wd/hub");
+                        driverPool.set(new RemoteWebDriver(url, chromeOptions));
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 default:
                     throw new RuntimeException("Wrong browser name!");
